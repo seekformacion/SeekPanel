44c0d7d842b96c78e9080d8a409f39fc7faaf7b1
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-15 13:21:08 +0100

    sss

--- allsites/ajx/fbtest.php
+++ allsites/ajx/fbtest.php
@@ -1,4 +1,10 @@
 <div style="width: 800px; height:30px; background-color:#E7EBF2; border:1px solid #C4CDE0; ">Test definitivo</div>
 <?php
 
+print_r($_POST);
+
+echo "<br><br><br>";
+
+print_r($_GET);
+
 ?>
