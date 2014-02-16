46664d0abda17731f9f8c0e464a8e1a933781c59
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-16 18:09:28 +0100

    ddd

--- allsites/ajx/fbtest.php
+++ allsites/ajx/fbtest.php
@@ -2,7 +2,7 @@
 
 ######### add boton   https://www.facebook.com/dialog/pagetab?api_key=715730281795141&next=https%3A%2F%2Fwww.facebook.com%2F
 
-########## params https://www.facebook.com/cursodecursos?v=app_622071311181276&app_data=jajajajaj
+########## params https://www.facebook.com/cursodecursos?v=app_715730281795141&app_data=jajajajaj
 
 print_r($_GET);
 
@@ -22,8 +22,8 @@ if ($like_status) {$stat="Te gusta";}else{$stat="Aun no te gusta";}
 
 
 $app_data = '';
-if(isset($signed_request["app_data"])){
-    $app_data = $signed_request["app_data"];
+if(isset($signed_request["referer"])){
+    $referer = $signed_request["referer"];
     }
 
 
@@ -31,4 +31,35 @@ if(isset($signed_request["app_data"])){
 
 
 
-<div style="width: 800px; height:30px; background-color:#E7EBF2; border:1px solid #C4CDE0;"> <?php echo $stat . " $app_data";?> </div>
+<div style="width: 800px; height:30px; background-color:#E7EBF2; border:1px solid #C4CDE0;"> <?php echo $stat . " $referer";?> </div>
+
+
+
+<script>
+	function getremotecookie() {
+
+	var surl =  "http://cursodecursos.com:8080/ajx/session.php?callback=?"; 
+	var me = $(this); 
+	$.getJSON(surl,  function(rtndata) { 
+	var cookie=rtndata.message;
+	
+	var csin=cookie.replace('||new','');
+	//console.log(csin);
+	
+	if(csin.length < cookie.length){
+	cookie=csin;
+	window.top.accept=1;	
+	}
+	
+	setCookie("seekforReferal",document.referrer,365);
+	setCookie("seekforID",cookie,365);
+	window.top.ckk=cookie;
+	initCurSEL();chkCsels(); 
+	checkGEOip();lcurSOC();
+ });
+ 
+}
+
+getremotecookie();	
+	
+</script>
