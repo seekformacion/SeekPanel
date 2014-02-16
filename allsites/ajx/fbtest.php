a0cbcee1ceb6580edba2bc5e272931102e615af9
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-16 16:48:46 +0100

    fff

--- allsites/ajx/fbtest.php
+++ allsites/ajx/fbtest.php
@@ -1,12 +1,15 @@
 <?php
+
+######### add boton   https://www.facebook.com/dialog/pagetab?api_key=715730281795141&next=https%3A%2F%2Fwww.facebook.com%2F
+
 ########## params https://www.facebook.com/cursodecursos?v=app_622071311181276&app_data=jajajajaj
 
 
 
 require '/www/repositorios/facebook-php-sdk/src/facebook.php';
 
-$app_id = "622071311181276";
-$app_secret = "24c6ad4ef66b34ec0fd74021bfd0fb5a";
+$app_id = "715730281795141";
+$app_secret = "59d82a1fcc819fc6579aba37ad1ec2c7";
 $facebook = new Facebook(array(
 'appId' => $app_id,
 'secret' => $app_secret,
