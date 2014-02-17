cfd901c4197d8dd6a28542c498c2bab57394d0cf
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-17 17:19:32 +0100

    www

--- allsites/ajx/ciphone-IL.php
+++ allsites/ajx/ciphone-IL.php
@@ -7,4 +7,4 @@
 
 
 <script type="text/javascript" src="<?php echo $http_met;?>://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo $http_met;?>://seekformacion.com/ajx/fbjs.js"></script>
+<script type="text/javascript" src="<?php echo $http_met;?>://seekformacion.com/ajx/fbjs.php"></script>
