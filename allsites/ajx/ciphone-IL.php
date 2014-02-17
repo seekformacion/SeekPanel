78ebb97f1ea93dc4d8d8967687c25ce7319c1cf8
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-17 14:33:28 +0100

    ddd

--- allsites/ajx/ciphone-IL.php
+++ allsites/ajx/ciphone-IL.php
@@ -1,17 +1,17 @@
-te gusta
 <?php
 
 $user = $facebook->getUser();
   if ($user) {
         $user_profile = $facebook->api('/me');
         $friends = $facebook->api('/me/friends');
-  
-  
+         print_r($user_profile);
   }else{
   
-  $login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_stream,publish_actions' ) );
+  $login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_stream,publish_actions', 'next' => "$http_met://www.facebook.com/cursodecursos/app_715730281795141" ) );
 
-	
+  header("Location: $login_url");	
 }
+
+
 
 ?>
