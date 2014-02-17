d610cb37e66ed7bd2e731e583645bfc167426bb6
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-17 15:55:53 +0100

    ddd

--- allsites/ajx/fblog.php
+++ allsites/ajx/fblog.php
@@ -10,10 +10,12 @@ $facebook->setAccessToken($code);
 $user = $facebook->getUser();
 echo "$user <br>";
 
+$rd="http://seekformacion.com/ajx/fblog.php";
+$rdU=urlencode($rd);
 
 if(!$user){
 
-$login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_stream,publish_actions') );
+$login_url = $facebook->getLoginUrl( array( "redirect_uri" => $rdU, 'scope' => 'publish_stream,publish_actions') );
 echo $login_url;
 
 }else{
@@ -23,7 +25,18 @@ print_r($user);
 }
 
 
+$rd="http://seekformacion.com/ajx/fblog.php";
+$rdU=urlencode($rd);
+
+
+echo "https://graph.facebook.com/oauth/access_token?client_id=715730281795141&redirect_uri=$rdU&client_secret=59d82a1fcc819fc6579aba37ad1ec2c7&code=$code";
+
+
  /*
+  * 
+https://graph.facebook.com/oauth/access_token?client_id=715730281795141&redirect_uri=FACEBOOK_REDIRECT_URI&client_secret=59d82a1fcc819fc6579aba37ad1ec2c7&code=TOKEN_VALUE
+  *  
+ 
 $accessToken = $facebook->getAccessToken();
 $facebook->setAccessToken($accessToken);
 
