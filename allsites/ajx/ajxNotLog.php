79ddb5c53fe317199fd5390c8a7a3f129c33dcc8
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-17 18:22:04 +0100

    hh

--- allsites/ajx/ajxNotLog.php
+++ allsites/ajx/ajxNotLog.php
@@ -6,7 +6,7 @@ foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo .
 
 
 <div style='cursor:pointer; width:150px; height:30px; background-color: blue;' 
-onclick='window.open("<?php echo $url;?>","","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=420, height=335, top=185, left=180");\' >
+onclick='window.open("<?php echo $url;?>","","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=420, height=335, top=185, left=180")'>
 
 
 </div>	
