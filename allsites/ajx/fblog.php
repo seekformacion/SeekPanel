6cd2add3ebb87e269380613288ce0248ecbd0f52
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-17 15:17:42 +0100

    ddd

--- allsites/ajx/fblog.php
+++ allsites/ajx/fblog.php
@@ -1,13 +1,48 @@
+aaa
 <?php
 
 require '/www/repositorios/facebook-php-sdk/src/facebook.php';
-require '/www/httpd/seekformacion.com/fbdata.php';
+//require '/www/httpd/seekformacion.com/fbdata.php';
 
+
+$app_id = "715730281795141";
+$app_secret = "59d82a1fcc819fc6579aba37ad1ec2c7";
+$facebook = new Facebook(array(
+    'appId' => $app_id,
+    'secret' => $app_secret,
+    'cookie' => true
+));
+
+
+$accessToken = $facebook->getAccessToken();
+echo "$accessToken <br>";
+
+$user = $facebook->getUser();
+
+
+
+if(!$user){
+
+$login_url = $facebook->getLoginUrl( array( 'scope' => 'publish_stream,publish_actions') );
+echo $login_url;
+
+}else{
+
+print_r($user);
+	
+}
+
+
+ /*
+$accessToken = $facebook->getAccessToken();
+$facebook->setAccessToken($accessToken);
 
 $user = $facebook->getUser();
 
 $permissions = $facebook->api("/me/permissions");
 
 print_r($user);
+*/
+
 
 ?>
