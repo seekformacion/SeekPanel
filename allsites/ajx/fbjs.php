d405965bd0e12410dadabf3e1b60d410adf801c5
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-17 17:24:34 +0100

    d

--- allsites/ajx/fbjs.php
+++ allsites/ajx/fbjs.php
@@ -1,8 +1,13 @@
+<?php
+foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
 
+
+
+?>
 
 function getId(){
 	
-var url='<?php echo $http_met;?>://seekformacion.com/ajx/fblog.php?aT=<?php echo $accessToken;?>';
+var url='<?php echo $http_met;?>://seekformacion.com/ajx/fblog.php?aT=<?php echo $aT;?>';
 $.getJSON(url, function(data) {
 $.each(data, function(key, val) {
 
