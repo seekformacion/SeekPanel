408d732c7ffe711447a1abefad50c961358866c1
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-17 11:42:27 +0100

    ddd

--- allsites/ajx/fblikes.php
+++ allsites/ajx/fblikes.php
@@ -1,41 +1,51 @@
 <?php
+  // Remember to copy files from the SDK's src/ directory to a
+  // directory in your application on the server, such as php-sdk/
+  require_once('/www/repositorios/facebook-php-sdk/src/facebook.php');
 
-######### add boton   https://www.facebook.com/dialog/pagetab?api_key=715730281795141&next=https%3A%2F%2Fwww.facebook.com%2F
+  $config = array(
+    'appId' => '439386826192305',
+    'secret' => '8f802ef919d61d26708df95dbac11af2',
+    'allowSignedRequest' => false // optional but should be set to false for non-canvas apps
+  );
 
-########## params https://www.facebook.com/cursodecursos?v=app_715730281795141&app_data=jajajajaj
-
-if($_SERVER['HTTPS']=='on'){$http_met= "https";}else{$http_met= "http";} 
-
-require '/www/repositorios/facebook-php-sdk/src/facebook.php';
-
-
-
-
-$config = array();
-$config['appId'] = '439386826192305';
-$config['secret'] = '8f802ef919d61d26708df95dbac11af2';
-$config['fileUpload'] = false; // optional
-
-$facebook = new Facebook($config);
-
-
-//Get Access Token URL
-$access_token_url = "https://graph.facebook.com/oauth/access_token?client_id=".$config['appId']."&client_secret=".$config['secret']."&grant_type=client_credentials";
-//Get Access Token
-$access_token = file_get_contents($access_token_url);
-echo $access_token;
-
-
-print_r($facebook);
-
-$user = $facebook->getUser();
-if ($user) {$user_profile = $facebook->api('/me');};
-$id_user=$user_profile['id'];
-
-echo $id_user;
-
-//$response = $facebook->api("/$id_user/likes");
-//print_r($response);
-
-
+  $facebook = new Facebook($config);
+  $user_id = $facebook->getUser();
 ?>
+<html>
+  <head></head>
+  <body>
+
+  <?php
+    if($user_id) {
+
+      // We have a user ID, so probably a logged in user.
+      // If not, we'll get an exception, which we handle below.
+      try {
+
+        $user_profile = $facebook->api('/me','GET');
+        echo "Name: " . $user_profile['name'];
+
+      } catch(FacebookApiException $e) {
+        // If the user is logged out, you can have a 
+        // user ID even though the access token is invalid.
+        // In this case, we'll get an exception, so we'll
+        // just ask the user to login again here.
+        $login_url = $facebook->getLoginUrl(); 
+        echo 'Please <a href="' . $login_url . '">login.</a>';
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+    } else {
+
+      // No user, print a link for the user to login
+      $login_url = $facebook->getLoginUrl();
+      echo 'Please <a href="' . $login_url . '">login.</a>';
+
+    }
+
+  ?>
+
+  </body>
+</html>
+
