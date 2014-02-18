e02e311b9d5de66bea11ad402cecf941059fe316
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-18 20:08:11 +0100

    sss

--- allsites/ajx/fb/ciphone-IL.php
+++ allsites/ajx/fb/ciphone-IL.php
@@ -7,7 +7,7 @@
 
 
 <script type="text/javascript" src="<?php echo $http_met;?>://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo $http_met;?>://seekformacion.com/ajx/fb/fbjs.php?http_met=<?php echo $http_met;?>&aT=<?php echo $accessToken;?>"></script>
+<script type="text/javascript" src="<?php echo $http_met;?>://seekformacion.com/ajx/fb/fbjs.js?http_met=<?php echo $http_met;?>&aT=<?php echo $accessToken;?>"></script>
 
 <img style="position:absolute; top:0px; left:0px;" border="0" src="http://seekformacion.com/img/<?php echo $portal;?>/fb/AppleCabe.gif">
 
