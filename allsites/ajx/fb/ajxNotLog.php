be3e018ebd87044612aa112bd4f29e7977e5162f
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-18 23:03:50 +0100

    dd

--- allsites/ajx/fb/ajxNotLog.php
+++ allsites/ajx/fb/ajxNotLog.php
@@ -6,7 +6,7 @@ if($_SERVER['HTTPS']=='on'){$http_met= "https";}else{$http_met= "http";}
 <div style="position:relative; top:0px; left:0px">
 <img src="<?php echo $http_met;?>://seekformacion.com/img/global/fb/activapanel.gif" border="0">	
 	
-<div style="position:absolute; top:250px; left:230px; cursor:pointer;" onclick='timer();window.open("<?php echo $url;?>","","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=420, height=335, top=185, left=180")'>
+<div style="position:absolute; top:250px; left:230px; cursor:pointer;" onclick='window.open("<?php echo $url;?>","","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=420, height=335, top=185, left=180"),timer();'>
 <img src="<?php echo $http_met;?>://seekformacion.com/img/global/fb/botAct.png" border="0">		
 </div>	
 
