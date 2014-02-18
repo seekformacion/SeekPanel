a15005d175dafcdc4953aaaa5bd801af8e12216e
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-18 20:07:28 +0100

    ddd

--- allsites/ajx/fb/ajxNotLog.php
+++ allsites/ajx/fb/ajxNotLog.php
@@ -5,7 +5,7 @@ $url=$_GET['url'];
 <div style="position:relative; top:0px; left:0px">
 <img src="http://seekformacion.com/img/global/fb/activapanel.gif" border="0">	
 	
-<div style="position:absolute; top:0px; left:200px; cursor:pointer;" onclick='window.open("<?php echo $url;?>","","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=420, height=335, top=185, left=180")'>
+<div style="position:absolute; top:250px; left:230px; cursor:pointer;" onclick='timer();window.open("<?php echo $url;?>","","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=420, height=335, top=185, left=180")'>
 <img src="http://seekformacion.com/img/global/fb/botAct.png" border="0">		
 </div>	
 
