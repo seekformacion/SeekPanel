76148ffd43b74dd9bc9f9c5b3d752c5e4bed592f
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-19 14:06:39 +0100

    s

--- allsites/ajx/fb/rank.php
+++ allsites/ajx/fb/rank.php
@@ -4,7 +4,7 @@
 require '/www/repositorios/facebook-php-sdk/src/facebook.php';
 require '/www/httpd/seekformacion.com/fbdata.php';
 
-if (isset($_COOKIE["seekforFB_FID"])){$user= $_COOKIE["seekforFB_FID"];};	
+if (isset($_COOKIE["seekforFB_ID"])){$user= $_COOKIE["seekforFB_ID"];};	
 
 echo "/$user/likes/1424213751150594";
 $response = $facebook->api("/$user/likes/1424213751150594");
