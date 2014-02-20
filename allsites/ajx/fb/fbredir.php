44ee013ad670a50ce3d4f88af923b2fe901ef6bf
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-20 22:20:00 +0100

    d

--- allsites/ajx/fb/fbredir.php
+++ allsites/ajx/fb/fbredir.php
@@ -1,11 +1,15 @@
 <?php
 
 
+print_r($_POST);
+
+print_r($_GET);
+
 
 ?>
 
 <script type="text/javascript">
-<!--
+
 window.location = "https://www.facebook.com/cursodecursos/app_715730281795141"
-//-->
+
 </script>
