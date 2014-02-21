b8e06fc16e98d7db4f32e349bb26e6482f409146
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-21 14:54:59 +0100

    sss

--- allsites/ajx/fb/fbjs.js
+++ allsites/ajx/fb/fbjs.js
@@ -217,7 +217,7 @@ FB.ui({
   method: 'apprequests',
   data: ref, 
   message: 'Ayudame a ganar el concurso Apple', 
-  filters: [{name: 'Amigos que no concursan', user_ids: ['' + filts + '']}]	
+  filters: [{name: 'Amigos que no concursan', user_ids: [100006051094341, 100007780393344]}]	
 });
 
 //filters: [{name: 'Amigos que aun no juegan', user_ids: [1018154356, 100007329815113]}]
