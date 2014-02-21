ae22d79f3b19de437473975209fc9dfcabb43ce2
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-21 12:13:04 +0100

    dddd

--- allsites/ajx/fb/fbredir.php
+++ allsites/ajx/fb/fbredir.php
@@ -17,16 +17,25 @@ foreach($request_ids as $request_id)
     {$request_object = $facebook->api($request_id);
      if(isset($request_object['data'])){
      	$req_data = $request_object['data']; //$req_data will be '12345' as per your request data set.
-		echo $req_data . "<br>"; 
+		//echo $req_data . "<br>"; 
      } 
     }
 
 
 
+
+
+if (!isset($_COOKIE["seekforFB_REFDE"])){
+$expire=time()+60*60*24*2;
+setcookie("seekforFB_REFDE", $req_data, $expire, '/');	
+}
+
+$url="https://www.facebook.com/cursodecursos/app_715730281795141";
+
 ?>
 
 <script type="text/javascript">
 
-//top.location.href ="<?php echo $req_data;?>";
+//top.location.href ="<?php echo $url;?>";
 
 </script>
