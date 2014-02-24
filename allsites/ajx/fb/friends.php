973817e2d9d267c43070050cf8be5afdd9a8602a
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-24 12:20:50 +0100

    rrr

--- allsites/ajx/fb/friends.php
+++ allsites/ajx/fb/friends.php
@@ -1,9 +1,29 @@
+<?php
 
+if (isset($_COOKIE["seekforFB_PID"])){
+$ref=$_COOKIE["seekforFB_PID"];	
+}
+
+?>
 <img src="https://seekformacion.com/img/global/fb/friends.gif" style="float: left;    left: 84px;    position: absolute;    top: 0px; " border="0">
 
-<div style="position: absolute; top:100px; background-color: orange; width:150px; height: 40px; left:120px; cursor:pointer;"></div>
+<div style="  cursor: pointer;
+    height: 40px;
+    left: 92px;
+    position: absolute;
+    top: 115px;
+    width: 251px;" onclick="postWall();"></div>
 
-<div style="position: absolute; top:100px; background-color: orange; width:150px; height: 40px; left:420px; cursor:pointer;" onclick="FacebookInviteFriends();"></div>
+<div style="   cursor: pointer;
+    height: 40px;
+    left: 428px;
+    position: absolute;
+    top: 114px;
+    width: 250px;" onclick="FacebookInviteFriends();"></div>
+    
+    
+    
+<div style="left: 158px;position: absolute;top: 314px; font-size: 15px; font-family: Arial; color: blue">http://cursodecursos.com/ajx/cApple.php?ref=<?php echo $ref;?></div>    
 
 
 
