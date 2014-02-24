5158a35bc004a744efd4849d3efb8d2f427ac796
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-02-24 12:04:21 +0100

    ssss

--- allsites/ajx/fb/friends.php
+++ allsites/ajx/fb/friends.php
@@ -1,10 +1,10 @@
 
-<img src="https://seekformacion.com/img/global/fb/friends.gif" style="" border="0">
+<img src="https://seekformacion.com/img/global/fb/friends.gif" style="float: left;    left: 84px;    position: absolute;    top: 0px; " border="0">
+
+<div style="position: absolute; top:100px; background-color: orange; width:150px; height: 40px; left:120px; cursor:pointer;"></div>
+
+<div style="position: absolute; top:100px; background-color: orange; width:150px; height: 40px; left:420px; cursor:pointer;" onclick="FacebookInviteFriends();"></div>
 
 
-
-<a href='#' onclick="FacebookInviteFriends();"> 
-Facebook Invite Friends Link
-</a>
 
 
