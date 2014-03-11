a276d0904637057e081c22bdf2702990a6b96db9
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-11 23:42:20 +0100

    ddd

--- allsites/ajx/fb/fblog.php
+++ allsites/ajx/fb/fblog.php
@@ -54,7 +54,7 @@ echo "<script>window.close();</script>";
 $redirect="https://seekformacion.com/ajx/fb/fblog.php?do=out";
 //$redirect=urlencode($redirect);
 
-$login_url = $facebook->getLoginUrl( array( 'redirect_uri' => $redirect, 'scope' => 'email,education,work,publish_stream,publish_actions', 'display' => 'popup') );
+$login_url = $facebook->getLoginUrl( array( 'redirect_uri' => $redirect, 'scope' => 'email,user_education_history,user_work_history,publish_stream,publish_actions', 'display' => 'popup') );
 
 if(!$user){
 $res['log']= $login_url;
