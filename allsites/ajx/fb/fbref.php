41ae9ba461786e52895a70ab6932ddf5e89285cd
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-25 18:55:55 +0100

    ddd

--- allsites/ajx/fb/fbref.php
+++ allsites/ajx/fb/fbref.php
@@ -13,17 +13,17 @@ return $code;
 }
 
 
-function create_new_user(){
-if (isset($_COOKIE["seekforFB_REFDE"])){$REF= $_COOKIE["seekforFB_REFDE"];}
+function create_new_user($REF){
 $PID=strtoupper(getUniqueCode(9));
 $res=DBUpIns("INSERT INTO Fb_fans (PID,REF) values ('$PID','$REF');");
 return $PID;
 }
 
 
-$val['PID']=create_new_user();
+$val['PID']=create_new_user($ref);
 
 echo json_encode($val);
 
-
+//http://www.seekformacion.com.com/ajx/fb/cApple.php?ref=5F5F46423609445E310C59A2FEA4CDDB&idp=1
 ?>
+
