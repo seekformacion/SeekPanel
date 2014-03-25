c43acf1c8eee2f1047d09e8bd70b0e874f9c2349
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-25 12:33:51 +0100

    s

--- allsites/ajx/fb/ajxNotLog.php
+++ allsites/ajx/fb/ajxNotLog.php
@@ -3,12 +3,34 @@ $url=$_GET['url'];
 if($_SERVER['HTTPS']=='on'){$http_met= "https";}else{$http_met= "http";} 
 ?>
 
+
+<style>
+.bases {position:absolute; top:380px; left:0px; border: 1px solid #888888; }	
+</style>
+
+<script>
+	function bases(){
+	document.getElementById('bases').style.visibility='block';	
+	}
+</script>
+
 <div style="position:relative; top:0px; left:0px">
 <img src="<?php echo $http_met;?>://seekformacion.com/img/global/fb/activapanel.gif" border="0">	
 	
 <div style="position:absolute; top:250px; left:230px; cursor:pointer;" onclick='window.open("<?php echo $url;?>","","toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=490, height=400, top=185, left=180"),timer();'>
 <img src="<?php echo $http_met;?>://seekformacion.com/img/global/fb/botAct.png" border="0">		
 </div>	
+
+<div style="position:absolute; top:350px; left:0px; font-family: Arial; font-size: 10px; color:#666666;">
+* Al activar el panel de control, estás aceptando las <span style="text-decoration: underline;" onclick="bases();">normas de uso y bases legales</span> del concurso.	
+</div>
+
+<div style="display:none" id="bases" class="bases">
+<iframe width="500" scrolling="auto" height="350" frameborder="0" style="display: block;" class="poli" src="/ajx/fb/bases.php" id="poli" border="0" marginheight="0">
+
+
+</iframe>
+</div>
 
 
 <div style="position:absolute; top:250px; left:230px; visibility: hidden;" id="timer" >
---
+++ allsites/ajx/fb/bases.php
@@ -0,0 +1,3 @@
+<div style="font-family: Arial; font-size: 10px; color:#666666;">
+
+</div>
