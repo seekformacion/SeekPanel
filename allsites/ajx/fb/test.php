1f41edd13fd91eef976d1c7efe9788eefab37dbf
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-26 18:00:27 +0100

    ss

--- allsites/ajx/fb/test.php
+++ allsites/ajx/fb/test.php
@@ -6,12 +6,15 @@ require '/www/httpd/seekformacion.com/fbdata.php';
 
 $user=$_GET('user');
 
+echo $user;
+
+
 $user_permissions = $facebook->api("/$user/permissions");
 
 
 $response = $facebook->api("/$user/likes/1424213751150594");
 
-print_r($user_profile);
+print_r($user_permissions);
 
 
 print_r($response);
