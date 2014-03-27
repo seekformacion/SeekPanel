0874437da68f3004284b4fd2ee902214e8aa4af2
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-27 13:52:56 +0100

    ggg

--- allsites/ajx/fb/fblog.php
+++ allsites/ajx/fb/fblog.php
@@ -39,7 +39,7 @@ $ins=DBUpIns("UPDATE Fb_fans SET PROF='$prof' WHERE FID='$user';");
 }
 
 
-if(isset($user_permissions["data"][0]["publish_actions"])){
+if(isset($user_permissions["data"][0]["email"])){
 setcookie("seekforFB_PEM", 2, $expire, '/');		
 }else{
 setcookie("seekforFB_PEM", 1, $expire, '/');	
