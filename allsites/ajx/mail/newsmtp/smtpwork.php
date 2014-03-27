ac6ee5e4192fef707012be6bfbc396a594ea50cc
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-27 21:47:54 +0100

    d

--- allsites/ajx/mail/newsmtp/smtpwork.php
+++ allsites/ajx/mail/newsmtp/smtpwork.php
@@ -27,15 +27,15 @@ echo "dddd";
 	$smtp->host_name="mrpotato.theservercluster.com"; //IP address       /* Change this variable to the address of the SMTP server to relay, like "smtp.myisp.com" */
 	$smtp->host_port=465;                /* Change this variable to the port of the SMTP server to use, like 465 */
 	$smtp->ssl=1;                       /* Change this variable if the SMTP server requires an secure connection using SSL */
-	$smtp->start_tls=1;                 /* Change this variable if the SMTP server requires security by starting TLS during the connection */
-	$smtp->localhost="localhost";       /* Your computer address */
+	$smtp->start_tls=0;                 /* Change this variable if the SMTP server requires security by starting TLS during the connection */
+	$smtp->localhost="54.194.56.56";       /* Your computer address */
 	$smtp->direct_delivery=0;           /* Set to 1 to deliver directly to the recepient SMTP server */
 	$smtp->timeout=10;                  /* Set to the number of seconds wait for a successful connection to the SMTP server */
 	$smtp->data_timeout=0;              /* Set to the number seconds wait for sending or retrieving data from the SMTP server.
 	                                       Set to 0 to use the same defined in the timeout variable */
 	$smtp->debug=1;                     /* Set to 1 to output the communication with the SMTP server */
 	$smtp->html_debug=1;                /* Set to 1 to format the debug output as HTML */
-	$smtp->pop3_auth_host="mrpotato.theservercluster.com";           /* Set to the POP3 authentication host if your SMTP server requires prior POP3 authentication */
+	$smtp->pop3_auth_host="";           /* Set to the POP3 authentication host if your SMTP server requires prior POP3 authentication */
 	$smtp->user="contenidos@seekformacion.com";                     /* Set to the user name if the server requires authetication */
 	$smtp->realm="";                    /* Set to the authetication realm, usually the authentication user e-mail domain */
 	$smtp->password="seek2014";                 /* Set to the authetication password */
