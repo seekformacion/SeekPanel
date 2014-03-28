002eb80ad9882a99eadc3da3856c980920500efa
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-28 11:41:21 +0100

    rrr

---
+++ allsites/scripts/testm.php
@@ -0,0 +1,55 @@
+<?php
+set_time_limit(0);
+ini_set("memory_limit", "-1");
+
+
+include('/www/dbP.php');
+
+$v['debug']=0;
+$v['admin']=0;
+
+$v['conf']['state']=1; # 1=test 2=produccion
+$v['conf']['mode']=1; # 1=local 2=cloud
+$v['where']['idp']=						1; #### ID DEL PORTAL PARA TABLA urls
+$v['where']['view']=					'scripts'; #### ID DEL PORTAL PARA TABLA urls
+$v['where']['id']=					    '1'; 
+$v['where']['site']=					"seekformacion.com";
+
+
+
+$v['path']['bin']=$v['path']['repo'] .	"/SeekPanel";
+$v['path']['fw']=$v['path']['repo'] .	"/FrameW_1";
+$v['path']['img']=$v['path']['repo'] .	"/SeekFormacion_images";
+
+$v['path']['baseURLskin'][1]=""; ## baseURL del SKIN local
+$v['path']['baseURLskin'][2]="http://s3-eu-west-1.amazonaws.com/seekf"; ## baseURL del SKIN en CLOUD
+
+
+
+
+ 
+require_once $v['path']['fw'] . '/core/templates/paths.php';
+
+includeINIT('vars');
+includeINIT('provins');
+includeINIT('config');
+includeCORE('db/dbfuncs');
+includeCORE('templates/templates');
+includeCORE('mail/mail');
+
+
+
+
+
+
+
+$from="contenidos@seekformacion.com";
+$to="e.b.moya@gmail.com";
+$subject="test defi";
+$message="prueba defi";
+
+sendM($from,$to,$subject,$message);
+
+
+
+?>
