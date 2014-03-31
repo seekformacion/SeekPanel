e36e7f871a6b9183106b15bdde50c55570368a96
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-03-31 12:31:18 +0200

    ddd

---
+++ allsites/ajx/activos.php
@@ -0,0 +1,23 @@
+<?
+################# basico
+$v['where']['view']='panel';
+$v['where']['id']=1; 
+require_once ('iniAJX.php');		//$v['conf']['db']="seekformacion"; // cargo otra bd
+##########################
+
+
+
+
+$inf=DBselect("select * from skP_users where id > 3 order by activo DESC;");
+
+
+echo "Activos <br>"; $f=0;
+foreach ($inf as $ky => $vall){
+
+if(($activo==0)&&(!$f)){$f++;echo "<br>Pendientes <br>";}
+
+$user=$vall['user']; $activo=$vall['activo'];
+	
+echo "$user <br>";	
+	
+}
