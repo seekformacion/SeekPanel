1c633896d7f801fecb76fff13040362bd038e328
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-08 21:08:03 +0200

    dddd

---
+++ allsites/ajx/fb3/noparticipa.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<style>
+.gris1_BG{  background-color: #EBEBEB !important;}	
+.page {width: 736px; background-color: #ffffff;
+    height: 600px;
+    margin: 0 auto;
+    padding: 0;}
+    	
+</style>
+
+
+
+<meta name="viewport" content="width=736px" />
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
+<title>Concurso Apple</title>
+</head>
+
+<body class="gris1_BG">
+	
+<div class="page">
+	
+	
+	
+</div>
+
+
+
+</body>
+</html>
---
+++ allsites/ajx/fb3/seekapp.php
@@ -0,0 +1,29 @@
+<?php
+header('P3P: CP="NOI ADM DEV COM NAV OUR STP"');
+$code="";$do="";$user=0;
+foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
+$expire=time()+60*60*24*2;
+
+
+require '/www/repositorios/facebook-php-sdk/src/facebook.php';
+require '/www/httpd/seekformacion.com/fbdata.php';
+
+
+$loginUrl = $facebook->getLoginUrl();
+$user = $facebook->getUser();
+if(!$user){header("Location: $loginUrl");}else{
+
+$user_permissions = $facebook->api("/$user/permissions");
+if(isset($user_permissions["data"][0]["user_likes"])){$participa=1;}else{$participa=0;}
+
+//print_r($user_permissions);
+//echo $user;
+
+if(!$participa){include('participa.php');}else{include('noparticipa.php');};
+
+}
+
+
+
+
+?>
