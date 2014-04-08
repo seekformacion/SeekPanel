d585880523174e2622698130075a336617c4fa1d
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-08 16:03:23 +0200

    tttt

--- allsites/ajx/fb3/test.php
+++ allsites/ajx/fb3/test.php
@@ -34,7 +34,7 @@
    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
    if (d.getElementById(id)) {return;}
    js = d.createElement('script'); js.id = id; js.async = true;
-   js.src = "//connect.facebook.net/en_US/all.js";
+   js.src = "//connect.facebook.net/es_ES/all.js";
    ref.parentNode.insertBefore(js, ref);
   }(document));
 
