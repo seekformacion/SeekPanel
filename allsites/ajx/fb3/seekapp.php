5c6d842c954c47bcc51aa84c95b36e2e4173c175
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-08 23:48:54 +0200

    sss

--- allsites/ajx/fb3/seekapp.php
+++ allsites/ajx/fb3/seekapp.php
@@ -1,5 +1,5 @@
 <?php
-//header('P3P: CP="NOI ADM DEV COM NAV OUR STP"');
+header('P3P: CP="NOI ADM DEV COM NAV OUR STP"');
 $code="";$do="";$user=0;
 foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
 $expire=time()+60*60*24*2;
@@ -11,24 +11,6 @@ require '/www/httpd/seekformacion.com/fbdata.php';
 
 $loginUrl = $facebook->getLoginUrl();
 $user = $facebook->getUser();
-
-
-if((!$user)&&($code)){
-
-$getaT= "https://graph.facebook.com/oauth/access_token?client_id=$app_id&redirect_uri=http%3A%2F%2Fseekformacion.com%2Fajx%2Ffb3%2Fseekapp.php&client_secret=$app_secret&code=$code";
-$content = file_get_contents($getaT);
-
-echo "<a href='$getaT'>$getaT</a> ";
-
-
-$datos=explode('&',$content);
-foreach($datos as $key => $valor){ if(strlen($valor) > strlen(str_replace('access_token=','',$valor)) ) { $access_token=str_replace('access_token=','',$valor);} };
-$facebook->setAccessToken($access_token);
-$user = $facebook->getUser();	
-}
-
-echo $user;
-
 if(!$user){
 	
 ?>
@@ -62,7 +44,7 @@ if(!$user){
 	
 	function login(){
 	
-	//window.location='<?php echo $loginUrl; ?>';	
+	window.location='<?php echo $loginUrl; ?>';	
 		
 	}
 	
@@ -75,7 +57,7 @@ if(!$user){
 
 <body class="gris1_BG" onload="login();">
 
-<a href='<?php echo $loginUrl;?>'><?php echo $loginUrl;?></a> ";
+
 
 </body>
 </html>
