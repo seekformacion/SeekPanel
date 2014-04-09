1dbc20e22f002a9b10876c4b1623681bbd9be469
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-09 22:41:21 +0200

    eee

--- allsites/ajx/fb3/noparticipa.php
+++ allsites/ajx/fb3/noparticipa.php
@@ -6,6 +6,14 @@ if(count($inf)>0){ foreach ($inf as $key => $value) {$FID=$value['FID'];
 $ultis .="<a href='http://www.facebook.com/$FID'><img class='imgUlti' src='https://graph.facebook.com/$FID/picture'></a>";	
 }}
 
+
+
+$redirect="$http_met://www.seekformacion.com/ajx/fb3/seekapp.php?do=perm";
+//$redirect=urlencode($redirect);
+$perm_url = $facebook->getLoginUrl( array( 'redirect_uri' => $redirect, 'scope' => 'basic_info,user_likes', 'display' => 'popup') );
+
+
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -22,14 +30,105 @@ $ultis .="<a href='http://www.facebook.com/$FID'><img class='imgUlti' src='https
     
     }
 
-.textos {position: relative; float:left; width: 696px; font-size: 15px; font-family: Arial; color:#555555 }
+.textos {position: relative; float:left; width: 696px; font-size: 16px; font-family: Arial; color:#555555 }
 .legal {position: relative; float:left; width: 696px; font-size: 9px; font-family: Arial; color:#555555; margin-top:5px; }
 
 .franjaAz {position: relative; float:left; width: 666px; background-color:#617087;  height: 130px;  margin-top: 60px; padding:15px; margin-bottom: 40px;}
 .ultis { position:relative; float:left;  font-size: 13px; font-family: Arial; color:#ffffff; width: 666px; margin-bottom:10px;}    	
 
 .imgUlti {position:relative; float:left; width:46px; height:46px; margin-right: 6px; }
+
+.bases {position:relative; float:left; top:50px;  border: 1px solid #888888; }	
+
 </style>
+
+<script>
+
+
+
+function setCookie(c_name,value,exdays)
+{
+var exdate=new Date();
+exdate.setDate(exdate.getDate() + exdays);
+if(exdays==0){
+var c_value=escape(value);
+}else{
+var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());	
+}
+
+document.cookie=c_name + "=" + c_value + '; path=/';
+}
+
+
+
+
+function getCookie(w){
+	cName = "";
+	pCOOKIES = new Array();
+	pCOOKIES = document.cookie.split('; ');
+	for(bb = 0; bb < pCOOKIES.length; bb++){
+		NmeVal  = new Array();
+		NmeVal  = pCOOKIES[bb].split('=');
+		if(NmeVal[0] == w){
+			cName = unescape(NmeVal[1]);
+		}
+	}
+	return cName;
+}
+
+
+
+
+
+function PopupCenter(pageURL, title,w,h) {
+  var left = (screen.width/2)-(w/2);
+  var tops = (screen.height/2)-(h/2);
+
+
+  var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+tops+', left='+left);
+timer();
+
+} 
+
+function timer(){
+document.getElementById('timer').style.visibility = "visible" ;
+setCookie('perm',0,20);
+chk1();	
+}	
+
+function getperm(){
+var a=getCookie('perm');	
+return a;
+}
+
+
+function chk1(){
+var func='chk2();'
+var st=getperm();
+
+if(st==0){setTimeout(func, 3000);}
+if(st==1){document.getElementById('timer').style.visibility = "hidden" ;}
+if(st==2){panel();}	
+
+
+}
+
+function chk2(){
+var func='chk1();'
+var st=getperm();
+
+if(st==0){setTimeout(func, 3000);}
+if(st==1){document.getElementById('timer').style.visibility = "hidden" ;}
+if(st==2){panel();}	
+
+	
+}
+
+function panel(){
+window.location="/ajx/fb3/seekapp.php";
+}
+	
+</script>
 
 
 
@@ -66,22 +165,29 @@ Todos los participantes en el concurso, participareis también en un sorteo con 
 
 <div style="clear:both"></div>
 
-<img style="position: relative; float: left; margin-top: 15px; cursor:pointer;" src="/img/global/contest/botParti.png">
+<img style="position: relative; float: left; margin-top: 15px; cursor:pointer;" src="/img/global/contest/botParti.png" onclick='PopupCenter("<?php echo $perm_url;?>","",490,400);'>
+
+<img style="visibility: hidden" class="timer" src="/img/global/contest/botParti.png" id="timer" />
 
 <img style="position: absolute; left: 425px; top:-90px  " src="/img/global/contest/premiosG.png">
 </div>
 	
 <div class="legal">
-* Al participar, estás aceptando las normas de uso y bases legales del concurso. Promoción válida para residentes en territorio español.	
+* Al participar, estás aceptando las <span style="text-decoration: underline; cursor:pointer;" onclick="javascript:document.getElementById('bases').style.display='block';">normas de uso y bases legales</span> del concurso. Promoción válida para residentes en territorio español.	
 </div>	
 	
 <div class="legal">
 ** Facebook no patrocina, avala ni administra de modo alguno esta promoción, ni está asociado a ella. Esta promoción esta avalada por Seek Formación S.L.	
 </div>		
 	
+
+
+<div style="display:none" id="bases" class="bases">
+<iframe width="696" scrolling="auto" height="250" frameborder="0" style="display: block; " class="poli" src="/ajx/fb3/bases.php" id="poli" border="0" marginheight="20" marginwidth="20">
+
+</iframe>
 </div>
 
-
-
+</div>
 </body>
 </html>
--- allsites/ajx/fb3/seekapp.php
+++ allsites/ajx/fb3/seekapp.php
@@ -1,90 +1,77 @@
 <?php
-header('P3P: CP="NOI ADM DEV COM NAV OUR STP"');
+//header('P3P: CP="NOI ADM DEV COM NAV OUR STP"');
+$expire=time()+60*60*24*2;
+
+if($_SERVER['HTTPS']=='on'){$http_met= "https";}else{$http_met= "http";} 
 
 $code="";$do="";$user=0;
 foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
 $expire=time()+60*60*24*2;
 
-global $_REQUEST;
 
 
 require '/www/repositorios/facebook-php-sdk/src/facebook.php';
 require '/www/httpd/seekformacion.com/fbdata.php';
 
 
-$loginUrl = $facebook->getLoginUrl();
 
+if($do=='perm'){ ########## callback de login
+
+	$user = $facebook->getUser();
+	if(!$user){
+	$loginUrl = $facebook->getLoginUrl();
+	header("Location: $loginUrl");
+	}else{
+	$user_permissions = $facebook->api("/$user/permissions");
+	if(isset($user_permissions["data"][0]["user_likes"])){
+	setcookie("perm", 2, $expire, '/');		
+	}else{
+	setcookie("perm", 1, $expire, '/');	
+	}
+	echo "<script>window.close();</script>";
+	}
+
+
+
+
+}else{
+	
+	
+	
+$loginUrl = $facebook->getLoginUrl();
 $user = $facebook->getUser();
 
 
 if(!$user){
+
+if(isset($_COOKIE["perm"])){
 	
-?>
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<style>
-
-.gris1_BG{  background-color: #EBEBEB !important;}	
-.page {width: 700px; background-color: #ffffff;
-    height: 1000px;
-    margin: 0 auto;
-    padding: 0px 18px 0px 18px;
-    
-    }
-
-.textos {position: relative; float:left; width: 696px; font-size: 15px; font-family: Arial; color:#555555 }
-.legal {position: relative; float:left; width: 696px; font-size: 9px; font-family: Arial; color:#555555; margin-top:5px; }
-
-.franjaAz {position: relative; float:left; width: 666px; background-color:#617087;  height: 130px;  margin-top: 60px; padding:15px; margin-bottom: 40px;}
-.ultis { position:relative; float:left;  font-size: 13px; font-family: Arial; color:#ffffff; width: 666px; margin-bottom:10px;}    	
-
-.imgUlti {position:relative; float:left; width:46px; height:46px; margin-right: 6px; }
-</style>
-
-<script>
+		if($_COOKIE["perm"]==2){
+			
+		header("Location: $loginUrl");
 	
-	function login(){
-	
-	window.location='<?php echo $loginUrl; ?>';	
-		
+		}else{
+		include('noparticipa.php');		
+		}	
+
+	}else{
+	include('noparticipa.php');		
 	}
-	
-</script>
 
-<meta name="viewport" content="width=736px" />
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
-<title>Concurso Apple</title>
-</head>
-
-<body class="gris1_BG" onload="login();">
-
-
-
-</body>
-</html>
-
-
-<?php	
-	
-	
 }else{
-
+	
 $user_permissions = $facebook->api("/$user/permissions");
-if(isset($user_permissions["data"][0]["user_likes"])){$participa=1;}else{$participa=0;}
-
-//print_r($user_permissions);
-//echo $user;
-
-if(!$participa){include('noparticipa.php');}else{include('noparticipa.php');};
+if(isset($user_permissions["data"][0]["user_likes"])){
+		include('participa.php');
+}else{
+		include('noparticipa.php');
+}	
+	
 
 }
 
 
+}
 
 
 ?>
