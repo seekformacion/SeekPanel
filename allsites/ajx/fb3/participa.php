a54d3d7301792f774b71d8f67c586917c11c161b
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-11 14:04:46 +0200

    sss

--- allsites/ajx/fb3/participa.php
+++ allsites/ajx/fb3/participa.php
@@ -132,7 +132,7 @@ $ranking=$rank[$PTOT];
 </style>
 
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo $http_met;?>://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
 <script>
 
