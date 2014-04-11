a4df233b6d148ea63952490b15b479e9ea72fb66
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-11 20:37:54 +0200

    dd

--- allsites/ajx/fb3/noparticipa.php
+++ allsites/ajx/fb3/noparticipa.php
@@ -126,7 +126,7 @@ if(st==2){panel();}
 
 function panel(){
 document.getElementById('page').innerHTML='<img style="position:absolute; top:150px; left:350px;" src="/img/global/contest/timer.gif">'	
-window.location="<?php echo $loginUrl;?>";
+setTimeout('window.location="<?php echo $loginUrl;?>"', 1200);';
 }
 	
 </script>
