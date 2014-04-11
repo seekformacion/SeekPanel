0cbe18d97745e2a86e2d25978493219289645c24
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-11 19:08:38 +0200

    s

---
+++ allsites/ajx/sendF.php
@@ -0,0 +1,29 @@
+<?php
+
+
+foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
+
+################# basico
+$v['where']['view']='panel';
+$v['where']['id']=1; 
+require_once ('iniAJX.php');
+
+includeCORE('mail/mail');
+
+$from="contenidos@seekformacion.com";
+$fromN="Alertas WEB";
+
+$to="e.b.moya@gmail.com";
+$toN="Eduardo Buenadicha";
+
+$subject="Contacto WEB";
+$message="$nom <br>\n";
+$message.="$mail <br>\n";
+$message.="$tel <br>\n";
+$message.="$cent <br>\n";
+$message.="$web <br>\n";
+$message.="$coment <br>\n";
+
+sendM($from,$fromN,$to,$toN,$subject,$message);
+
+?>
