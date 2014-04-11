2cb6863d8d77e597d58357af3ae166440a71bf4f
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-11 14:15:39 +0200

    sss

--- allsites/ajx/fb3/seekapp.php
+++ allsites/ajx/fb3/seekapp.php
@@ -36,6 +36,28 @@ if($do=='perm'){ ########## callback de login
 
 }else{
 	
+
+
+$signed_request = $facebook->getSignedRequest();
+$request_ids = $_GET['request_ids'];
+$request_ids = explode(",", $request_ids);
+foreach($request_ids as $request_id)
+    {$request_object = $facebook->api($request_id);
+     if(isset($request_object['data'])){
+     	$req_data = $request_object['data']; //$req_data will be '12345' as per your request data set.
+		//echo $req_data . "<br>"; 
+     } 
+    }
+
+if (!isset($_COOKIE["seekforFB_REFDE"])){
+$expire=time()+60*60*24*2;
+setcookie("seekforFB_REFDE", $req_data, $expire, '/');	
+}
+
+
+
+
+
 	
 	
 $loginUrl = $facebook->getLoginUrl();
