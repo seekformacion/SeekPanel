0c33ed08ea9e509da2ef0704f927ddc7517534c9
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-23 12:36:51 +0200

    eee

--- allsites/ajx/pixel.php
+++ allsites/ajx/pixel.php
@@ -2,6 +2,15 @@
 
 print_r($_SERVER);
 
+$sname=$_SERVER['SCRIPT_NAME'];
+$sname=str_replace('/pixel/p_', '', $sname);
+$sname=str_replace('.gif', '', $sname);
+$datos=explode('_',$sname);
 
+$mail=$datos[0];
+$asunto=$datos[1];
+$id_bol=$datos[2];
+
+echo "$mail $asunto $id_bol";
 
 ?>
