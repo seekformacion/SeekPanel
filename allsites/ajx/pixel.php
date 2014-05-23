92d860c519b0d4f1876e7bfe45bd9b2677ab44ac
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-23 12:46:55 +0200

    jjj

--- allsites/ajx/pixel.php
+++ allsites/ajx/pixel.php
@@ -1,6 +1,5 @@
 <?php
-
-print_r($_SERVER);
+header( "Content-type: image/gif" );
 
 $sname=$_SERVER['SCRIPT_NAME'];
 $sname=str_replace('/pixel/p_', '', $sname);
@@ -11,6 +10,13 @@ $mail=$datos[0];
 $asunto=$datos[1];
 $id_bol=$datos[2];
 
-echo "$mail $asunto $id_bol";
+//echo "$mail $asunto $id_bol";
+
+$my_img = imagecreate( 650, 1 );
+$background = imagecolorallocate( $my_img, 255, 255, 255 );
+
+imagegif( $my_img );
+imagecolordeallocate( $background );
+imagedestroy( $my_img );
 
 ?>
