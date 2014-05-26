5571fede8d969ee70be34d36dea3d8a0e4081a83
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-26 12:45:10 +0200

    eee

---
+++ allsites/ajx/baja.php
@@ -0,0 +1,23 @@
+<?php
+
+################# basico
+$v['where']['view']='panel';
+$v['where']['id']=1; 
+require_once ('iniAJX.php');		//$v['conf']['db']="seekformacion"; // cargo otra bd
+includeCORE('funcs/funcSESSION');
+##########################
+
+$id=""; $bol="";
+if(array_key_exists('id', $_GET)){$id=$_GET['id'];}
+if(array_key_exists('bol', $_GET)){$bol=$_GET['bol'];}
+
+if(($id)&&($bol)){
+	
+	
+	
+	
+}
+
+
+echo "si";
+?>
