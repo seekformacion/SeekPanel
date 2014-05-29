e118d86cb08da977211768471a7007da76b42163
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-05-29 11:14:49 +0200

    dd

--- allsites/ajx/pixel.php
+++ allsites/ajx/pixel.php
@@ -1,6 +1,6 @@
 <?php
 
-$im = file_get_contents("/www/httpd/publiactive.net/amazonlogo.gif");
+$im = file_get_contents("/www/httpd/publiactive.net/pixelbol.gif");
 header("Content-type: image/gif");
 echo $im;
 
@@ -23,32 +23,9 @@ $v['where']['id']=1;
 require_once ('iniAJX.php');		$v['conf']['db']="SeekforFB"; // cargo otra bd
 //includeCORE('funcs/funcSESSION');
 ##########################
-//DBUpInsSDB("UPDATE envios SET open=1 WHERE id_boletin=$id_bol;",'SeekforFB');	
+DBUpInsSDB("UPDATE envios SET open=1 WHERE id_boletin=$id_bol;",'SeekforFB');	
 
-/* 
 
-$my_img = imagecreate( 650, 1 );
-$background = imagecolorallocate( $my_img, 255, 255, 255 );
-
-header( "Content-type: image/gif" );
-imagegif( $my_img );
-imagecolordeallocate( $my_img , $background );
-imagedestroy( $my_img );
-
-//$im = imagecreatetruecolor(650, 1);
-
-// sets background to red
-//$red = imagecolorallocate($im, 255, 255, 255);
-//imagefill($im, 0, 0, $red);
-
-//header('Content-type: image/gif');
-//imagegif($im);
-//imagedestroy($im);
-
- * 
- * 
- * 
- */
  
 
 
