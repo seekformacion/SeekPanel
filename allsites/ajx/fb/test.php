faedb19047711fe6c9c10ae8c32447f40fcc3162
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-06-24 19:15:04 +0200

    eee

--- allsites/ajx/fb/test.php
+++ allsites/ajx/fb/test.php
@@ -5,7 +5,7 @@ $user=$_GET['user'];
 require '/www/repositorios/facebook-php-sdk/src/facebook.php';
 require '/www/httpd/seekformacion.com/fbdata.php';
 
-
+/*
 
 $fql = "SELECT user_id FROM url_like WHERE url='http://masterenmasters.com/masters-en-direccion-de-operaciones.html' AND user_id = $user;";
 $response = $facebook->api(array(
@@ -14,10 +14,13 @@ $response = $facebook->api(array(
 ));
 
 print_r($response);
-
+*/
 
 $app_access_token = $app_id . '|' . $app_secret;
+$ap=DBselect("SELECT FID FROM Fb_fans WHERE puntos >= 400;");
+foreach ($ap as $key => $value) {$user=$value['FID'];
 
+/*
 $response = $facebook->api( "/$user/notifications", 'POST', array(
 
                 'template' => 'Nueva forma de obtener puntos',
@@ -30,6 +33,16 @@ $response = $facebook->api( "/$user/notifications", 'POST', array(
             ) );    
 
 print_r($response);
+*/
+
+echo $user . " <br>\n";
+	
+}
+
+
+
+
+
 
 
 ?>
