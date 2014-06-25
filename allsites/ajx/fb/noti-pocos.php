f70a3f4666b23a3928750dcac31ca8532d18310b
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-06-25 17:58:02 +0200

    www

--- allsites/ajx/fb/noti-pocos.php
+++ allsites/ajx/fb/noti-pocos.php
@@ -22,7 +22,7 @@ $ap=DBselect("SELECT FID, puntos FROM Fb_fans WHERE puntos < 400 AND env=0;");
 
 $ap=array();
 $ap[1]['FID']=100007329815113;
-$ap[1]['puntos']=0;
+$ap[1]['puntos']=10;
 
 foreach ($ap as $key => $value) {$user=$value['FID']; $puntos=$value['puntos'];
 
@@ -31,7 +31,7 @@ DBUpIns("UPDATE Fb_fans SET env=1 WHERE FID=$user;");
 if($puntos==0){
 $mens="Para participar en el sorteo necesitas tener algún punto.";	
 }else{
-$mens="Tienes muy pocos puntos. Cuantos más puntos, más posibilidades en el sorteo.";		
+$mens="Tienes muy pocos puntos. Cuantos más puntos, más posibilidades tendrás en el sorteo.";		
 }
 
 $response = $facebook->api( "/$user/notifications", 'POST', array(
