ff22ff0b418d3e527b4854ac1312ef9016643952
eduardo <e.b.moya@gmail.com>
2014-10-23 12:24:14 +0200

    eded

---
+++ allsites/ajx/fb3/sendvalidation.php
@@ -0,0 +1,33 @@
+<?php
+
+foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";   eval($asignacion);};
+
+################# basico
+$v['where']['view']='panel';
+$v['where']['id']=1; 
+require_once ('../iniAJX.php');        $v['conf']['db']="SeekforFB"; // cargo otra bd
+includeCORE('funcs/funcSESSION');
+##########################
+
+includeCORE('mail/mail');
+
+$user="15254451551"; $email="e.b.moya@gmail.com"; $PID="668B968EE7C191492D50900075863805";
+
+$sed=DBUpIns("UPDATE Fb_fans SET v_email='$email' WHERE PID='$PID';");
+
+$from="concurso@publiactive.es";
+$fromN="Validación";
+
+$to="e.b.moya@gmail.com";
+$toN="Eduardo Buenadicha";
+
+$subject="Validación del perfil";
+
+$message=loadChild('mails','validacion_concurso');
+$plain="Este mensaje es para validar el perfil";
+
+$vconf=array();
+
+echo sendM($from,$fromN,$to,$toN,$subject,$message,$plain,'mailCDC.php',$vconf);
+
+?>
