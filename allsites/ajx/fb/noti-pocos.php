a258d1bc34708192aa488ffe03c3f9511aaa9de2
Eduardo Buenadicha <e.b.moya@gmail.com>
2014-10-30 19:19:59 +0100

    ttt

--- allsites/ajx/fb/noti-pocos.php
+++ allsites/ajx/fb/noti-pocos.php
@@ -6,6 +6,11 @@ ini_set("memory_limit", "-1");
 require '/www/repositorios/facebook-php-sdk/src/facebook.php';
 require '/www/httpd/seekformacion.com/fbdata.php';
 
+$users_ban[545870963]=1;
+$users_ban[100001770109514]=1;
+$users_ban[638135366]=1;
+
+
 /*
 
 $fql = "SELECT user_id FROM url_like WHERE url='http://masterenmasters.com/masters-en-direccion-de-operaciones.html' AND user_id = $user;";
@@ -18,41 +23,37 @@ print_r($response);
 */
 
 $app_access_token = $app_id . '|' . $app_secret;
-$ap=DBselect("SELECT FID, puntos FROM Fb_fans WHERE puntos < 1001 AND env=0;");
+$ap=DBselect("SELECT FID, puntos FROM Fb_fans WHERE puntos > 0 AND env=0;");
 
 
-/*
+
 $ap=array();
 $ap[1]['FID']=100007329815113;
 $ap[1]['puntos']=10;
-*/
+$ap[2]['FID']=545870963;
+$ap[1]['puntos']=10;
+
 
 
 foreach ($ap as $key => $value) {$user=$value['FID']; $puntos=$value['puntos'];
-
 DBUpIns("UPDATE Fb_fans SET env=1 WHERE FID=$user;");	
 
-if($puntos==0){
-$mens="Para participar en el sorteo necesitas tener algún punto.";	
-}else{
-$mens="Recuerda, cuantos más puntos obtengas más posibilidades tendrás en el sorteo.";		
-}
+$mens="La promoción ha finalizado";
 
+if(!array_key_exists($user, $users_ban)){
 $response = $facebook->api( "/$user/notifications", 'POST', array(
-
                 'template' => $mens,
-
                 'href' => 'newpuntos',
-
                 'access_token' => $app_access_token
-
-                
-            ) );    
+                        ) );    
 
 print_r($response);
+echo $user . " \n";
+}else{
+echo $user . " \n";    
+}
 
 
-echo $user . " <br>\n";
 	
 }
 
